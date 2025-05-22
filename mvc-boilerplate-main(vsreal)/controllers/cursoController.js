2de2ed9cc90e55bf754aba949ff375df3c31e8c8
belapecanha <isabela.pecanha@sou.inteli.edu.br>
2025-05-22 12:00:26 -0300

    aula6

--- mvc-boilerplate-main(vsreal)/controllers/cursoController.js
+++ mvc-boilerplate-main(vsreal)/controllers/cursoController.js
@@ -5,14 +5,14 @@ exports.create = async (req, res) => {
   await Curso.create(nome);
   res.redirect('/alunos');
 };
-// Controller que recebe os dados do formulário e chama o model para atualizar o curso
+
 exports.update = async (req, res) => {
   const { id } = req.params;
   const { nome } = req.body;
   await Curso.update(id, nome);
   res.redirect('/alunos');
 };
-// Controller que chama o model para deletar o curso e redireciona
+
 exports.delete = async (req, res) => {
   const { id } = req.params;
   await Curso.delete(id);
